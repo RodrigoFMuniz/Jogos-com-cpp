269aa75ba1d6821abeacd9182fa6c82e30810690
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-30 01:32:10 -0300

    First commit

---
+++ primeiroCodigo/game.cpp
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+int APIENTRY WinMain( HINSTANCE hInstance,
+                      HINSTANCE hPrevInstance,
+                      LPSTR lpCmdLine, 
+                      int nCmdShow)
+{
+    MessageBox(NULL,"Hello World!","Texto",0);
+
+    return 0;
+
+}
